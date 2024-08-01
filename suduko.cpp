6098941d38c25bae318ef26ce50474c5daf9395b
Ankur Pandey <109913061+tickLESTOK@users.noreply.github.com>
2024-08-01 20:43:39 +0530

    Update suduko.cpp

--- suduko.cpp
+++ suduko.cpp
@@ -1,17 +1,15 @@
-
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <array>
 using std::cout;
 using std::endl;
+
 #define DIM 9
 #define BLANK 0
-#define SPACE " "
-#define LINE "|"
+#define SPACE ' '          // Use a single character for space
+#define LINE '|'
 #define NEW_ROW "-------------------------------------"
-#define GRID_FULL std::make_pair(9, 9)
-// Prints the Soduko grid
+
+// Prints the Sudoku grid
+
 void print_grid(int grid[DIM][DIM])
 {
 	for (int i = 0; i < DIM; i++)
@@ -35,126 +33,95 @@ void print_grid(int grid[DIM][DIM])
 	}
 	cout << endl << NEW_ROW << endl << endl;;
 }
-// Returns a boolean which indicates whether any assigned entry
-// in the specified row matches the given number. 
-bool used_in_row(int grid[DIM][DIM], int row, int num)
+
+// Checks if a number is used in the specified row
+bool used_in_row(const int grid[DIM][DIM], int row, int num)
 {
-	for (int col = 0; col < DIM; col++)
-		if (grid[row][col] == num)
-		{
-			return true;
-		}
-	return false;
+    for (int col = 0; col < DIM; col++)
+        if (grid[row][col] == num)
+            return true;
+    return false;
 }
-// Returns a boolean which indicates whether any assigned entry
-// in the specified column matches the given number. 
-bool used_in_col(int grid[DIM][DIM], int col, int num)
+
+// Checks if a number is used in the specified column
+bool used_in_col(const int grid[DIM][DIM], int col, int num)
 {
-	for (int row = 0; row < DIM; row++)
-		if (grid[row][col] == num)
-		{
-			return true;
-		}
-	return false;
+    for (int row = 0; row < DIM; row++)
+        if (grid[row][col] == num)
+            return true;
+    return false;
 }
-// Returns a boolean which indicates whether any assigned entry
-// within the specified 3x3 box matches the given number. 
-bool used_in_box(int grid[DIM][DIM], int box_start_rpw, int box_start_col, int num)
+
+// Checks if a number is used in the 3x3 box starting at (box_start_row, box_start_col)
+bool used_in_box(const int grid[DIM][DIM], int box_start_row, int box_start_col, int num)
 {
-	for (int row = 0; row < 3; row++)
-		for (int col = 0; col < 3; col++)
-			if (grid[row + box_start_rpw][col + box_start_col] == num) 
-			{
-				return true;
-			}
-	return false;
+    for (int row = 0; row < 3; row++)
+        for (int col = 0; col < 3; col++)
+            if (grid[box_start_row + row][box_start_col + col] == num)
+                return true;
+    return false;
 }
-// Returns a boolean which indicates whether it will be legal to assign
-// num to the given row,col location.
-bool is_safe(int grid[DIM][DIM], int row, int col, int num)
+
+// Checks if it's safe to place a number at (row, col)
+bool is_safe(const int grid[DIM][DIM], int row, int col, int num)
 {
-	// Check if 'num' is not already placed in current row,
-	// current column and current 3x3 box 
-	return !used_in_row(grid, row, num) &&
-		!used_in_col(grid, col, num) &&
-		!used_in_box(grid, row - row % 3, col - col % 3, num);
+    int box_start_row = row - row % 3;
+    int box_start_col = col - col % 3;
+    return !used_in_row(grid, row, num) &&
+           !used_in_col(grid, col, num) &&
+           !used_in_box(grid, box_start_row, box_start_col, num);
 }
-// Searches the grid to find an entry that is still unassigned. If
-// found, the reference parameters row, col will be set the location
-// that is unassigned, and true is returned. If no unassigned entries
-// remain, false is returned. 
-std::pair<int, int> get_unassigned_location(int grid[DIM][DIM])
+
+// Finds an unassigned location in the grid
+bool get_unassigned_location(const int grid[DIM][DIM], int &row, int &col)
 {
-	for (int row = 0; row < DIM; row++)
-		for (int col = 0; col < DIM; col++)
-			if (grid[row][col] == BLANK)
-			{
-				return std::make_pair(row, col);
-			}
-	return GRID_FULL;
+    for (row = 0; row < DIM; row++)
+        for (col = 0; col < DIM; col++)
+            if (grid[row][col] == BLANK)
+                return true;
+    return false;
 }
-// Takes a partially filled-in grid and attempts to assign values to
-// all unassigned locations in such a way to meet the requirements
-// for Sudoku solution (non-duplication across rows, columns, and boxes) 
-bool solve_soduko(int grid[DIM][DIM])
+
+// Solves the Sudoku using backtracking
+bool solve_sudoku(int grid[DIM][DIM])
 {
-	// If the Soduko grid has been filled, we are done
-	if (GRID_FULL == get_unassigned_location(grid))
-	{
-		return true; 
-	}
-	// Get an unassigned Soduko grid location
-	std::pair<int, int> row_and_col = get_unassigned_location(grid);
-	int row = row_and_col.first;
-	int col = row_and_col.second;
-	// Consider digits 1 to 9
-	for (int num = 1; num <= 9; num++)
-	{
-		// If placing the current number in the current
-		// unassigned location is valid, go ahead
-		if (is_safe(grid, row, col, num))
-		{
-			// Make tentative assignment
-			grid[row][col] = num;
-			// Do the same thing again recursively. If we go 
-			// through all of the recursions, and in the end 
-			// return true, then all of our number placements 
-			// on the Soduko grid are valid and we have fully
-			// solved it
-			if (solve_soduko(grid))
-			{
-				return true;
-			}
-			// As we were not able to validly go through all 
-			// of the recursions, we must have an invalid number
-			// placement somewhere. Lets go back and try a 
-			// different number for this particular unassigned location
-			grid[row][col] = BLANK;
-		}
-	}
-	// If we have gone through all possible numbers for the current unassigned
-	// location, then we probably assigned a bad number early. Lets backtrack 
-	// and try a different number for the previous unassigned locations.
-	return false; 
+    int row, col;
+
+    if (!get_unassigned_location(grid, row, col))
+        return true; // No unassigned location means the puzzle is solved
+
+    for (int num = 1; num <= 9; num++)
+    {
+        if (is_safe(grid, row, col, num))
+        {
+            grid[row][col] = num;
+            if (solve_sudoku(grid))
+                return true;
+            grid[row][col] = BLANK; // Backtrack
+        }
+    }
+    return false;
 }
+
 int main()
 {
-	cout << "********************************\n\n\tSudoku Solver\n\n********************************" << endl << endl;
+    cout << "********************************\n\n\tSudoku Solver\n\n********************************" << endl << endl;
 
-	int grid[DIM][DIM] = { { 0, 9, 0, 0, 0, 0, 8, 5, 3 },
-						   { 0, 0, 0, 8, 0, 0, 0, 0, 4 },
-						   { 0, 0, 8, 2, 0, 3, 0, 6, 9 },
-						   { 5, 7, 4, 0, 0, 2, 0, 0, 0 },
-						   { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-						   { 0, 0, 0, 9, 0, 0, 6, 3, 7 },
-						   { 9, 4, 0, 1, 0, 8, 5, 0, 0 },
-						   { 7, 0, 0, 0, 0, 6, 0, 0, 0 },
-						   { 6, 8, 2, 0, 0, 0, 0, 9, 0 } };
-	
+    int grid[DIM][DIM] = {
+        {0, 9, 0, 0, 0, 0, 8, 5, 3},
+        {0, 0, 0, 8, 0, 0, 0, 0, 4},
+        {0, 0, 8, 2, 0, 3, 0, 6, 9},
+        {5, 7, 4, 0, 0, 2, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 9, 0, 0, 6, 3, 7},
+        {9, 4, 0, 1, 0, 8, 5, 0, 0},
+        {7, 0, 0, 0, 0, 6, 0, 0, 0},
+        {6, 8, 2, 0, 0, 0, 0, 9, 0}
+    };
 
-	    print_grid(grid);
+    print_grid(grid);
 
-    if (solve_soduko(grid))
+    if (solve_sudoku(grid))
     {
         cout << "Sudoku solved successfully:" << endl;
         print_grid(grid);
@@ -164,5 +131,4 @@ int main()
         cout << "No solution exists!" << endl;
     }
     return 0;
-
 }
